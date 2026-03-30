d848014c67cc6fc3972b75f332aa6af5abc4ed93
LolPaGo <lolpago@example.com>
2026-03-30 14:12:13 +0900

    add Google AdSense meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,7 @@ export default function RootLayout({
           }}
         />
         <meta name="google-site-verification" content="hsjncRi9cl3tz3Otd6SJKurSt_V1bZ0AKO-bdWIGeHM" />
+        <meta name="google-adsense-account" content="ca-pub-4361110443201092" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
